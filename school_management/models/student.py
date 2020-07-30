dddb7cefb9c94610379f89b7dc6628d82bf24464
Shivam <shivamshastri123@gmail.com>
2020-07-30 22:03:32 +0530

    Update 07/30/20

--- school_management/models/student.py
+++ school_management/models/student.py
@@ -7,11 +7,33 @@ class Student(models.Model):
     _description = 'My Student Module'
 
     student_name = fields.Char(string="Student Name")
+    student_surname = fields.Char(string="Student Surname")
+    age = fields.Integer(string='Age')
+    scholarship = fields.Boolean(string="Scholarship?")
     student_address = fields.Text(string='student_address')
-    student_marks = fields.Float(string="Student Marks")
+    student_marks = fields.Float(string="Student Percentage")
+    student_Physics_marks = fields.Float(string="Student Physics marks")
+    student_Chemistry_marks = fields.Float(string="Student Chemistry Marks")
+    student_Math_marks = fields.Float(string="Student Math Marks")
     currently_studying = fields.Boolean(string="Currently studying?")
+    medical_problems = fields.Text(string='Medical problems if any')
+    blood_group = fields.Selection([ ('o+', 'O Positive'),('a+', 'A Positive'),('b+', 'B Positive'),('AB+', 'AB Positive')], string='Blood Group')
     join_date = fields.Date(string="Join Date")
-    s_photo = fields.Image(string='Student Photo')
-    signed_on = fields.Datetime(string='Signed On', help="Date of the signature.",copy=False)
+    s_photo = fields.Image(string="Student Photo")
+    signed_on = fields.Datetime(string="Signed On", help="Date of the signature.",copy=False)
     s_selection = fields.Selection([ ('sel1', 'Semester 1'),('sel2', 'Semester 2'),('sel3', 'Semester 3'),('sel4', 'Semester 4')], string='Semester Selection')
     sequence = fields.Integer(string='Sequence', default=10)
+
+    state = fields.Selection([
+        ('studying', 'Still Studying'),
+        ('done', 'Graduated'),
+    ], string='Selection Status', readonly=True, copy=False, store=True, default='studying')
+
+
+    def action_state_graduate(self):
+    	self.write({'state': 'done'})
+    	self.write({'currently_studying': False})
+
+    def action_state_enroll(self):
+    	self.write({'state': 'studying'})
+    	self.write({'currently_studying': True})
--- school_management/models/teacher.py
+++ school_management/models/teacher.py
@@ -8,10 +8,29 @@ class Teacher(models.Model):
 
     teacher_name = fields.Char(string="Teacher Name", required=True)
     teacher_surname = fields.Text(string='Teacher_Surname')
+    teacher_address = fields.Text(string='Teacher_Address')
+    age = fields.Integer(string="Age")
     teacher_salary = fields.Float(string="Teacher Salary")
     in_service = fields.Boolean(string="Currently in service?")
     join_date = fields.Date(string="Join Date")
     t_photo = fields.Image(string='Teacher Photo')
     signed_on = fields.Datetime(string='Signed On', help="Date of the signature.",copy=False)
     t_selection = fields.Selection([ ('tsel1', 'Primary'),('tsel2', 'Secondary'),('tsel3', 'Higher Secondary')], string='Teaching teaching level')
+    state = fields.Selection([
+        ('draft', 'Not Working'),
+        ('done', 'Hired'),
+    ], string='Selection Status', readonly=True, copy=False, store=True, default='draft')
     sequence = fields.Integer(string='Teacher', default=10)
+    govt_certified = fields.Boolean(string="Govt approved?")
+    phd	= fields.Boolean(string="PhD passout?")
+    blood_group = fields.Selection([ ('o+', 'O Positive'),('a+', 'A Positive'),('b+', 'B Positive'),('AB+', 'AB Positive')], string='Blood Group')
+
+
+    def action_go_forward(self):
+    	self.write({'state': 'done'})
+    	self.write({'in_service': True})
+
+
+    def action_go_backward(self):
+    	self.write({'state': 'draft'})
+    	self.write({'in_service': False})

f7fde322324472310b88629d33a6580fd8ea3045
Shivam <shivamshastri123@gmail.com>
2020-08-18 12:29:23 +0530

    18Aug20

--- hospital_management/models/patient.py
+++ hospital_management/models/patient.py
@@ -25,3 +25,71 @@ class Patient(models.Model):
         'treatment.treatment', 'patient_id', string="Treatment ID")
     my_treatment_id = fields.Many2one(
         'treatment.treatment', string="Treatment Id")
+
+    def smartbutton(self):
+        return {
+            'name': ('Treatment'),
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_model': 'treatment.treatment',
+            'view_id': False,
+            'type': 'ir.actions.act_window',
+            'target': 'new'
+        }
+
+    def smartbutton1(self):
+        return {
+            'name': ('Doctor'),
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_model': 'doctor.doctor',
+            'view_id': False,
+            'type': 'ir.actions.act_window',
+            'target': 'new'
+        }
+
+    def _find_mail_template(self, force_confirmation_template=False):
+        template_id = False
+        if force_confirmation_template:
+            template_id = int(self.env['ir.config_parameter'].sudo(
+            ).get_param('sale.default_confirmation_template'))
+            template_id = self.env['mail.template'].search(
+                [('id', '=', template_id)]).id
+            if not template_id:
+                template_id = self.env['ir.model.data'].xmlid_to_res_id(
+                    'sale.mail_template_sale_confirmation', raise_if_not_found=False)
+        if not template_id:
+            template_id = self.env['ir.model.data'].xmlid_to_res_id(
+                'sale.email_template_edi_sale', raise_if_not_found=False)
+
+        return template_id
+
+    def action_quotation_send(self):
+        self.ensure_one()
+        template_id = self._find_mail_template()
+        lang = self.env.context.get('lang')
+        template = self.env['mail.template'].browse(template_id)
+        if template.lang:
+            lang = template._render_template(
+                template.lang, 'sale.order', self.ids[0])
+        ctx = {
+            'default_model': 'sale.order',
+            'default_res_id': self.ids[0],
+            'default_use_template': bool(template_id),
+            'default_template_id': template_id,
+            'default_composition_mode': 'comment',
+            'mark_so_as_sent': True,
+            'custom_layout': "mail.mail_notification_paynow",
+            'proforma': self.env.context.get('proforma', False),
+            'force_email': True,
+            'model_description': self.with_context(lang=lang).type_name,
+        }
+        return {
+            'type': 'ir.actions.act_window',
+            'view_mode': 'form',
+            'res_model': 'mail.compose.message',
+            'views': [(False, 'form')],
+            'view_id': False,
+            'target': 'new',
+            'context': ctx,
+        }
--- hospital_management/wizard/patient_wizard.py
+++ hospital_management/wizard/patient_wizard.py
@@ -12,13 +12,18 @@ class PatientWizard(models.TransientModel):
         'treatment.treatment', string="Treatment Id")
 
     def action_process_patient(self):
-        parent_id = self._context.get('active_ids')
-        patient = self.env['patient.patient'].browse(parent_id)
-        print('patient', patient, self.treatment_id)
-        patient.my_treatment_id = self.treatment_id.id
-        print("aaaaaaaaaaaaaaaaaaaaaa", patient)
-        return True
+        return {
+            'name': ('Doctor'),
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_model': 'doctor.doctor',
+            'view_id': 'doctor_view_order_form',
+            'type': 'ir.actions.act_window',
+            'target': 'new'
+        }
 
-    # 	self.env['patient.patient'].search([('treatment_ids', '=', self.treatment_id),
-        #                               (field,'!=', False),
-        #                               (field,'!=', 0.0)]) #
+        #parent_id = self._context.get('active_ids')
+        #patient = self.env['patient.patient'].browse(parent_id)
+        #print('patient', patient, self.treatment_id)
+        #patient.my_treatment_id = self.treatment_id.id
+        #print("aaaaaaaaaaaaaaaaaaaaaa", patient)
